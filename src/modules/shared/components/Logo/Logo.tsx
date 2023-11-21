485aafb8c1976f4e324a77a8d4b9fb5661a62388
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-11-21 19:43:47 -0300

    Add tailwind-variants package

--- src/modules/shared/components/Logo/Logo.tsx
+++ src/modules/shared/components/Logo/Logo.tsx
@@ -1,23 +1,54 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import { VariantProps, tv } from 'tailwind-variants';
+
 import { useAppSafeArea } from '../../hooks/useAppSafeArea';
 
-interface LogoProps {
-  name: string;
+const $logo = tv({
+  slots: {
+    app: 'text-5xl italic font-bold',
+    name: 'text-5xl italic font-bold',
+  },
+
+  variants: {
+    type: {
+      todo: {
+        app: 'dark:text-blue-700',
+        name: 'text-blue-700 dark:text-white',
+      },
+
+      weather: {
+        app: 'dark:text-orange-600',
+        name: 'text-orange-600 dark:text-white',
+      },
+
+      store: {
+        app: 'dark:text-green-600',
+        name: 'text-green-600 dark:text-white',
+      },
+    },
+  },
+
+  defaultVariants: {
+    type: 'todo',
+  },
+});
+
+interface LogoProps extends VariantProps<typeof $logo> {
+  appName: string;
+  type?: 'todo' | 'weather' | 'store';
 }
 
-export function Logo({ name }: LogoProps) {
+export function Logo({ appName, type = 'todo' }: LogoProps) {
   const { top } = useAppSafeArea();
+
+  const { app, name } = $logo({ type });
 
   return (
     <View className="flex-row pb-4 justify-center" style={{ marginTop: top }}>
-      <Text className="text-5xl italic font-bold dark:text-blue-700">
-        {name}
-      </Text>
-      <Text className="text-5xl italic text-blue-700 font-bold dark:text-white">
-        App
-      </Text>
+      <Text className={app()}>{appName}</Text>
+      <Text className={name()}>App</Text>
     </View>
   );
 }
