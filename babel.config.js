1610b49d3b6a42dc1b3b656aad7ce4f268b57bf2
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-11-21 16:54:59 -0300

    Changed import config

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,32 @@ module.exports = {
   plugins: [
     'nativewind/babel',
     'react-native-reanimated/plugin',
-    ['module-resolver'],
+    [
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@dtos': './src/dtos',
+          '@components': './src/components',
+          '@todo': './src/modules/TodoApp',
+          '@todoComp': './src/modules/TodoApp/components',
+          '@weather': './src/modules/WeatherApp',
+          '@weatherComp': './src/modules/WeatherApp/components',
+          '@store': './src/modules/StoreApp',
+          '@storeComp': './src/modules/StoreApp/components',
+          '@shared': './src/modules/shared',
+          '@hooks': './src/hooks',
+          '@screens': './src/screens',
+          '@storage': './src/storage',
+          '@utils': './src/utils',
+          '@services': './src/modules/shared/services',
+          '@routes': './src/routes',
+          '@api': './src/api',
+          '@types': './src/types',
+          '@infra': './src/infra',
+          '@mock': './src/mock',
+        },
+      },
+    ],
   ],
 };
