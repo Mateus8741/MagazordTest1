768da996086debeb49b9384b2fec42b338167b91
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-11-21 14:06:53 -0300

    Add handleAddTask function to HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,11 +5,15 @@ import { Logo } from '../components/Logo/Logo';
 import { TextInput } from '../components/TextInput/TextInput';
 
 export function HomeScreen() {
+  function handleAddTask(task: string) {
+    console.log(task);
+  }
+
   return (
     <View className="flex-1 px-5 bg-white dark:bg-gray-900">
       <Logo />
 
-      <TextInput />
+      <TextInput onAddTask={task => handleAddTask(task)} />
 
       <Text>HomeScreen</Text>
     </View>
