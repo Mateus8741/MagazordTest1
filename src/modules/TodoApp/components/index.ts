bff0dc811f53031118fa25d4accceb8c9656dab8
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-11-21 19:56:34 -0300

    Refactor TextInput component and remove unused imports

--- src/modules/TodoApp/components/index.ts
+++ src/modules/TodoApp/components/index.ts
@@ -1,5 +1,3 @@
 /* eslint-disable prettier/prettier */
-export * from '../../shared/components/Logo/Logo';
 export * from './TaskItem/TaskItem';
-export * from './TextInput/TextInput';
 
--- src/modules/shared/components/TextInput/TextInput.tsx
+++ src/modules/shared/components/TextInput/TextInput.tsx
@@ -7,13 +7,55 @@ import {
   View,
 } from 'react-native';
 
-interface AddTaskProps extends RNInputProps {
-  onAddTask: (taskText: string) => void;
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { VariantProps, tv } from 'tailwind-variants';
+
+const $input = tv({
+  slots: {
+    input: 'border dark:border-white rounded-md p-3',
+    button: 'w-11 h-11 items-center justify-center rounded-md ml-3',
+  },
+
+  variants: {
+    type: {
+      todo: {
+        input: 'dark:text-white',
+        button: 'bg-blue-700 dark:decoration-sky-400',
+      },
+
+      weather: {
+        input: 'dark:text-white',
+        button: 'bg-orange-600 dark:decoration-orange-400',
+      },
+
+      store: {
+        input: 'dark:text-white',
+        button: 'bg-green-600 dark:decoration-green-400',
+      },
+    },
+  },
+
+  defaultVariants: {
+    type: 'todo',
+  },
+});
+
+interface AddTaskProps extends RNInputProps, VariantProps<typeof $input> {
+  onAddTask: (text: string) => void;
+  iconName: string;
+  type?: 'todo' | 'weather' | 'store';
 }
 
-export function TextInput({ onAddTask, ...rest }: AddTaskProps) {
+export function TextInput({
+  onAddTask,
+  iconName,
+  type = 'todo',
+  ...rest
+}: AddTaskProps) {
   const [isFocused, setIsFocused] = useState(false);
   const [taskText, setTaskText] = useState('');
+
+  const { input, button } = $input({ type });
 
   const focusedStyles = isFocused
     ? 'border-blue-700'
@@ -28,7 +70,7 @@ export function TextInput({ onAddTask, ...rest }: AddTaskProps) {
     <View className="flex-row items-center">
       <View className="flex-1">
         <RNInput
-          className={`border ${focusedStyles} rounded-md p-3`}
+          className={`${input()} ${focusedStyles}`}
           onFocus={() => setIsFocused(true)}
           onBlur={() => setIsFocused(false)}
           onChangeText={setTaskText}
@@ -37,10 +79,10 @@ export function TextInput({ onAddTask, ...rest }: AddTaskProps) {
         />
       </View>
 
-      <TouchableOpacity
-        className="w-11 h-11 items-center justify-center bg-blue-700 decoration-sky-400 rounded-md ml-3"
-        onPress={handleNewTask}>
-        <Text className="text-white text-3xl">+</Text>
+      <TouchableOpacity className={button()} onPress={handleNewTask}>
+        <Text className="text-white text-3xl">
+          <Icon name={iconName} size={24} />
+        </Text>
       </TouchableOpacity>
     </View>
   );
