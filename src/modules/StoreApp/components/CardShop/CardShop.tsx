7210d08e2b9329e886e055bce0e424d500b1d240
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-11-22 16:44:26 -0300

    Add CardShop components to StoreHomeScreen

---
+++ src/modules/StoreApp/components/CardShop/CardShop.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+
+import { useShadowProps } from '@shared';
+
+export function CardShop() {
+  const shadowProps = useShadowProps();
+
+  return (
+    <View
+      className="h-[170px] w-[170px] bg-gray-900 dark:bg-white rounded-lg overflow-hidden mb-6"
+      style={shadowProps}>
+      <Image
+        source={{ uri: 'https://picsum.photos/200/300' }}
+        className="flex-1"
+        resizeMode="cover"
+      />
+      <View className="px-3 py-1 justify-center bg-transparent">
+        <Text className="text-xl font-bold text-white dark:text-gray-900">
+          Teclado
+        </Text>
+        <Text className="text-base font-normal text-zinc-300 dark:text-zinc-700">
+          R$ 99,99
+        </Text>
+      </View>
+    </View>
+  );
+}
--- src/modules/StoreApp/screens/StoreHomeScreen/StoreHomeScreen.tsx
+++ src/modules/StoreApp/screens/StoreHomeScreen/StoreHomeScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 import { Logo, Screen } from '@shared';
+import { CardShop } from '@storeComp';
 
 export function StoreHomeScreen() {
   const variantType = 'store';
@@ -11,6 +12,15 @@ export function StoreHomeScreen() {
       <View className="flex-1 px-5">
         <Logo appName="Store" type={variantType} />
         <Text>StoreHomeScreen</Text>
+
+        <View className="flex-wrap flex-row justify-between">
+          <CardShop />
+          <CardShop />
+          <CardShop />
+          <CardShop />
+          <CardShop />
+          <CardShop />
+        </View>
       </View>
     </Screen>
   );
