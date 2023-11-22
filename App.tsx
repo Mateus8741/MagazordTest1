32e088374b3a81c47f575eecab4895995ecb8e5e
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-11-21 23:38:28 -0300

    Add @tanstack/react-query and @tanstack/eslint-plugin-query

--- App.tsx
+++ App.tsx
@@ -7,14 +7,19 @@
 
 import React from 'react';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { Routes } from '@routes';
 
 export function App(): JSX.Element {
+  const [queryClient] = React.useState(() => new QueryClient());
+
   return (
     <SafeAreaProvider>
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <Routes />
+      </QueryClientProvider>
     </SafeAreaProvider>
   );
 }
