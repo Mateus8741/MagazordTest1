50bea7205e6c45321e460dffac7aa0537d841db3
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-11-22 17:24:18 -0300

    Add empty state for product list

--- src/modules/StoreApp/screens/StoreHomeScreen/StoreHomeScreen.tsx
+++ src/modules/StoreApp/screens/StoreHomeScreen/StoreHomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 
 import { StoreDTO } from '@dtos';
 import { Logo, Screen, UseStoreApi } from '@shared';
@@ -14,6 +14,16 @@ export function StoreHomeScreen() {
     return <CardShop data={item} />;
   }
 
+  function renderEmpty() {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <Text className="text-lg font-bold text-gray-900 dark:text-white">
+          Nem um produto encontrado
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <Screen>
       <View className="flex-1 px-5">
@@ -23,11 +33,12 @@ export function StoreHomeScreen() {
           data={data?.data}
           keyExtractor={item => item.id.toString()}
           renderItem={({ item }) => renderItem(item)}
+          ListEmptyComponent={renderEmpty}
           numColumns={2}
+          showsVerticalScrollIndicator={false}
           columnWrapperStyle={{
             justifyContent: 'space-between',
           }}
-          showsVerticalScrollIndicator={false}
         />
       </View>
     </Screen>
