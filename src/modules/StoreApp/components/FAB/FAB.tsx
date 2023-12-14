9528b22fcc5ca778ca0f73a43ed4089c9c11fa57
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-12-14 12:15:16 -0300

    Refactor FAB component and add shopping cart icon

--- src/modules/StoreApp/components/FAB/FAB.tsx
+++ src/modules/StoreApp/components/FAB/FAB.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 interface FABProps extends TouchableOpacityProps {}
 
 export function FAB({ ...rest }: FABProps) {
   return (
     <TouchableOpacity
-      className="absolute bottom-12 right-4 w-14 h-14 border border-white shadow-md rounded-full bg-green-600 dark:bg-white dark:border-green-600 justify-center items-center"
+      className="absolute bottom-12 right-4 w-14 h-14 border border-white shadow-md rounded-full bg-green-600 dark:bg-green-800 justify-center items-center"
       {...rest}>
-      <Text className="text-white dark:text-green-600 text-2xl font-bold">
-        +
-      </Text>
+      <Icon name="shopping-cart" size={24} color="#FFF" />
     </TouchableOpacity>
   );
 }
